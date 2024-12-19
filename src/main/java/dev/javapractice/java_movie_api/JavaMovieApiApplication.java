c75db0ed959f10436f501d4ea442dfaa0fae1eec
Kent Tuneberg <ktunebe@gmail.com>
2024-12-19 15:03:49 -0600

    initial commit

---
+++ src/main/java/dev/javapractice/java_movie_api/JavaMovieApiApplication.java
@@ -0,0 +1,13 @@
+package dev.javapractice.java_movie_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMovieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMovieApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/javapractice/java_movie_api/JavaMovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.javapractice.java_movie_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
