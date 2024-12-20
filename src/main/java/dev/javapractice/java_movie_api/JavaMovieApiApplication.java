d24ee05cbcccc1a37cb3cd0308e5488113750633
Kent Tuneberg <ktunebe@gmail.com>
2024-12-20 14:42:35 -0600

    started setting up rest API

--- src/main/java/dev/javapractice/java_movie_api/JavaMovieApiApplication.java
+++ src/main/java/dev/javapractice/java_movie_api/JavaMovieApiApplication.java
@@ -2,12 +2,17 @@ package dev.javapractice.java_movie_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JavaMovieApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaMovieApiApplication.class, args);
 	}
+
+
 
 }
